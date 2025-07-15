a34816177bc1fd8a107fbb4830ff0e7068eb4a82
noahfng <nxoa10805@gmail.com>
2025-07-15 16:23:10 +0200

    Finished resolution correction

--- nSigma_Plot.C
+++ nSigma_Plot.C
@@ -2,6 +2,7 @@
 #include <vector>
 #include <limits>
 #include <cmath>
+#include <sstream>
 
 #include "TROOT.h"
 #include "TStyle.h"
@@ -9,13 +10,12 @@
 #include "TChain.h"
 #include "TCanvas.h"
 #include "TH1F.h"
-#include "TLegend.h"
-#include "TF1.h"  
-#include "TSpectrum.h"
-#include "TSystem.h"
-#include "RooFit.h"
+#include "TLine.h"
+#include "TLatex.h"
+#include "TPaveText.h"
+#include "TF1.h"
 
-#include <AddTrees.h>
+#include "AddTrees.h"
 
 Double_t bethe_bloch_aleph(Double_t bg, Double_t p1, Double_t p2, Double_t p3, Double_t p4, Double_t p5) {
     Double_t beta = bg / TMath::Sqrt(1.0 + bg*bg);
@@ -59,152 +59,146 @@ std::vector<double> topBinCenters(TH1 *h, int nWanted)
 void nSigma_Plot(){
     gROOT->SetBatch(kTRUE);
     gStyle->SetOptStat(1);
-    const char* baseDir = "/home/nfingerle/SMI/UD_LHC23_pass4_SingleGap/0106/B";
+    const char *baseDir = "/home/nfingerle/SMI/UD_LHC23_pass4_SingleGap/0106/B";
     TChain chain("twotauchain");
     AddTrees(chain, baseDir);
 
     chain.SetBranchStatus("*", 0);
     chain.SetBranchStatus("fTrkTPCinnerParam", 1);
-    const char* subs[5] = {"El","Mu","Pi","Ka","Pr"};
-    for (int i = 0; i < 5; ++i) {
-        chain.SetBranchStatus(Form("fTrkTPCnSigma%s", subs[i]), 1);}
+
+    const char *subs[5] = {"El", "Mu", "Pi", "Ka", "Pr"};
+    for (int i = 0; i < 5; ++i)
+        chain.SetBranchStatus(Form("fTrkTPCnSigma%s", subs[i]), 1);
+
     Float_t inner[2];
     Float_t tpcNS[5][2];
     chain.SetBranchAddress("fTrkTPCinnerParam", inner);
-    for (int i = 0; i < 5; ++i) {
-        chain.SetBranchAddress(Form("fTrkTPCnSigma%s", subs[i]), tpcNS[i]);}
+    for (int i = 0; i < 5; ++i)
+        chain.SetBranchAddress(Form("fTrkTPCnSigma%s", subs[i]), tpcNS[i]);
 
-    const Int_t nBins = 200;
-    const Double_t pMin = 0.5, pMax = 0.6; 
-    const Double_t xMin = -15.0, xMax = 15.0;
-    const Int_t nParts = 5;
-    const TString names[nParts] = {"e","#mu","#pi","K","p"};
-    const Int_t colors[nParts] = {kBlue, kBlueYellow, kGreen+2, kOrange+7, kViolet};
-    const Double_t resoTPC[5]   = {0.085, 0.072, 0.074, 0.09, 0.08}; 
-    const Double_t masses[nParts] = {0.00051099895, 0.1056583755,  0.13957039, 0.493677, 0.93827208816};
+    const Int_t   nBins   = 200;
+    const Double_t xMin   = -15.0, xMax = 15.0;
+    const Double_t pMin   = 0.5, pMax = 0.6; 
+    const Int_t   nParts  = 5;
+    const TString names[nParts]   = {"e", "#mu", "#pi", "K", "p"};
+    const Int_t   colors[nParts] = {kBlue, kBlueYellow, kGreen + 2,
+                                    kOrange + 7, kViolet};
+    const Double_t resoTPC[nParts] = {0.085, 0.072, 0.074, 0.090, 0.080};
+    const Double_t masses[nParts]  = {0.00051099895, 0.1056583755,
+                                      0.13957039,    0.493677,
+                                      0.93827208816};
 
-    TH1F *hRes[nParts];
-    for (int h = 0; h < nParts; ++h) {
-        hRes[h] = new TH1F(Form("n#sigma_{%s}",names[h].Data()), 
-        Form("n#sigma_{%s} for %.2f < p < %.2f GeV/c; n#sigma_{%s}; Counts", names[h].Data(),pMin, pMax, names[h].Data()),nBins, xMin, xMax);
-        hRes[h]->SetLineColor(colors[h]);
-        hRes[h]->SetMarkerColor(colors[h]);}
-    
-    Long64_t nEntries = chain.GetEntries();
-    nEntries = std::min(nEntries, static_cast<Long64_t>(1e6));
-    for(Long64_t i = 0; i < nEntries; ++i){
-        chain.GetEntry(i);
-        for(int j = 0; j < 2; ++j){
-            Float_t pG = inner[j]; 
-            if(pG < pMin || pG > pMax)   continue;
-            
-            for (int h = 0; h < nParts; ++h) {
-                if (!TMath::IsNaN(tpcNS[h][j])){
-                    hRes[h]-> Fill(tpcNS[h][j]);
-                }
-            }
-        }
-    }
+    Long64_t nEntries = std::min(chain.GetEntries(), static_cast<Long64_t>(1e6));
 
-    TCanvas *c = new TCanvas("c", "Residuals", 800, 600);
+    TCanvas *c = new TCanvas("c", "n#sigma histograms", 950, 700);
     c->SetLeftMargin(0.15);
     c->SetGrid();
-    c->Print("nSigmaTPC_plot.pdf[");
-    for (int h = 0; h < nParts; ++h) {
+    c->Print("nSigmaTPC_theorySeed.pdf[");
+
+
+    for (int ref = 0; ref < nParts; ++ref) {
+        TH1F *hist = new TH1F(Form("h_%s", names[ref].Data()),
+                Form("n#sigma_{%s}, %.2f < p < %.2f GeV/c; n#sigma_{%s}; Counts",
+                     names[ref].Data(), pMin, pMax, names[ref].Data()),
+                nBins, xMin, xMax);
+        hist->SetMarkerStyle(kFullCircle);
+        hist->SetMarkerSize(0.75);
+        hist->SetMarkerColor(kBlack);
+        hist->SetLineColor(kBlack);
+
+        for (Long64_t i = 0; i < nEntries; ++i) {
+            chain.GetEntry(i);
+            for (int t = 0; t < 2; ++t) {
+                Float_t pG = inner[t];
+                if (pG < pMin || pG > pMax) continue;
+                if (!TMath::IsNaN(tpcNS[ref][t])) hist->Fill(tpcNS[ref][t]);
+            }
+        }
+        const double pMid = 0.5 * (pMin + pMax);
+        const double dRef = get_expected_signal(pMid * 1000., masses[ref] * 1000, 1.0);
+
+        std::vector<double> muSeed;
+        std::vector<int>    hypId;
+
+        double yMax = 1.05 * hist->GetMaximum();
+        for (int hyp = 0; hyp < nParts; ++hyp) {
+            double dHyp = get_expected_signal(pMid * 1000., masses[hyp] * 1000, 1.0);
+            if (dRef < 0 || dHyp < 0) continue;
+
+            double mu = (dHyp / dRef - 1.0) / resoTPC[hyp];
+            if (mu < xMin || mu > xMax) continue;
+
+            TLine *tl = new TLine(mu, 0., mu, yMax);
+            tl->SetLineColor(colors[hyp]);
+            tl->Draw();
+            TLatex lab;
+            lab.SetTextColor(colors[hyp]);
+            lab.SetTextSize(0.03);
+            lab.SetTextAlign(22);
+            lab.DrawLatex(mu, 0.92 * yMax, names[hyp]);
+
+            muSeed.push_back(mu);
+            hypId.push_back(hyp);
+        }
+
+        const int nG = muSeed.size();
+        if (nG == 0) { c->Print("nSigmaTPC_theorySeed.pdf"); delete hist; continue; }
+
+        std::ostringstream form;
+        for (int i = 0; i < nG; ++i) {
+            if (i) form << "+";
+            form << "gaus(" << 3 * i << ")";
+        }
+        TF1 *sum = new TF1("sum", form.str().c_str(), xMin, xMax);
+
+        for (int i = 0; i < nG; ++i) {
+            int    bin = hist->FindBin(muSeed[i]);
+            double amp = hist->GetBinContent(bin);
+            sum->SetParameters(3 * i, amp, muSeed[i], 0.30);
+
+            sum->SetParLimits(3 * i + 0, 0.0, hist->GetMaximum() * 1.2);        // amp ≥ 0
+            sum->SetParLimits(3 * i + 1, muSeed[i] - 1.0, muSeed[i] + 1.0);      // |Δμ| ≤ 1σ
+            sum->SetParLimits(3 * i + 2, 0, 100.0);                             // reasonable σ
+        }
+
+        hist->Fit(sum, "QR0");
         c->Clear();
-        hRes[h]->SetMarkerStyle(kFullCircle);
-        hRes[h]->SetMarkerColor(kBlack);
-        hRes[h]->SetLineColor(kRed);
-        hRes[h]->SetMarkerSize(0.75); 
-        hRes[h]->Draw("E1");
+        hist->Draw("E1");
 
-        const double pMid = 0.5*(pMin + pMax);            
-        double mRef = masses[h]; 
-        for (int hyp = 0; hyp < 5; ++hyp) {
-            double mHyp = masses[hyp];
-            double dRef = get_expected_signal(pMid*1000, mRef*1000, 1.0);
-            double dHyp = get_expected_signal(pMid*1000, mHyp*1000, 1.0);                 
+        sum->SetLineColor(kRed);
+        sum->SetLineWidth(2);
+        sum->SetNpx(500);
+        sum->Draw("same");
 
-            double nSigmaPeak = (dHyp/dRef - 1.0)/ (resoTPC[hyp]);
-            std::cout << "nSigmaPeak for " << subs[hyp] << " = " << nSigmaPeak << std::endl;
-            double yMax = 1.05 * hRes[h]->GetMaximum();
-            TLine *l = new TLine(nSigmaPeak, 0.0, nSigmaPeak, yMax);
-            l->SetLineColor(colors[hyp]);
-            l->SetLineStyle(1);
-            l->Draw("same");
+        for (int i = 0; i < nG; ++i) {
+            double amp = sum->GetParameter(3 * i);
+            if (amp <= 0) continue;                  
 
-            TLatex txt;
-            txt.SetTextSize(0.035);
-            txt.SetTextAlign(22);                          
-            txt.SetTextColor(colors[hyp]);
-            txt.DrawLatex(nSigmaPeak, 0.9*yMax, subs[hyp]); 
+            TF1 *g = new TF1(Form("g_%d_%d", ref, i), "gaus", xMin, xMax);
+            g->SetParameters(&sum->GetParameters()[3 * i]);
+            g->SetLineColor(colors[hypId[i]]);
+            g->SetLineStyle(2);
+            g->Draw("same");
         }
 
-        const int nTryMax = 5;                         
-        TF1  *bestFit = nullptr;
-        double bestBIC = std::numeric_limits<double>::infinity();
-        int    bestN   = 0;
-
-        for (int nG = 1; nG <= nTryMax; ++nG) {
-
-            std::ostringstream form;
-            for (int i = 0; i < nG; ++i) {
-                if (i) form << "+";
-                form << "gaus(" << 3*i << ")";
-            }
-            TF1* fTmp = new TF1("fTmp", form.str().c_str(), xMin, xMax);
-
-            auto mu = topBinCenters(hRes[h], nG);
-            for (int i = 0; i < nG; ++i) {
-                int    bin = hRes[h]->FindBin(mu[i]);
-                double amp = hRes[h]->GetBinContent(bin);
-
-                fTmp->SetParameter(3*i+0, amp);          // amplitude  (p = 3i)
-                fTmp->SetParameter(3*i+1, mu[i]);        // mean       (p = 3i+1)
-                fTmp->SetParameter(3*i+2, 0.30);         // sigma      (p = 3i+2)
-
-                fTmp->SetParLimits(3*i+2, 0.05, 5.0);    
-            }
-
-            hRes[h]->Fit(fTmp, "QR0");                         
-            double chi2 = fTmp->GetChisquare();
-            int    k    = 3*nG;                                 
-            double nObs = hRes[h]->GetEntries();
-            double bic = chi2 +k * TMath::Log(nObs);
-            const double deltaBICmin = 2;
-
-            if (bestBIC - bic > deltaBICmin) {
-                bestBIC = bic;
-                bestN   = nG;
-                delete bestFit;
-                bestFit = (TF1*)fTmp->Clone(Form("best_%d",h));
-            }
-            else break;
+        for (size_t i = 0; i < muSeed.size(); ++i) {
+            TLine *tl = new TLine(muSeed[i], 0., muSeed[i], yMax);
+            tl->SetLineColor(colors[hypId[i]]);
+            tl->Draw();
         }
 
-        bestFit->SetLineColor(kRed);
-        bestFit->SetNpx(500);
-        bestFit->SetLineWidth(2);
-        bestFit->Draw("same");
-
-        //for (int i = 0; i < bestN; ++i) {
-        //    TF1* g = new TF1(Form("g_%d_%d",h,i),"gaus",xMin,xMax);
-        //    for (int p = 0; p < 3; ++p)
-        //        g->SetParameter(p, bestFit->GetParameter(3*i+p));
-        //    g->SetLineStyle(2);
-        //    g->SetLineColor(colors[i % nParts]);
-        //    g->Draw("same");
-        //}
-
-        double chi2 = bestFit->GetChisquare();
-        int    ndf  = bestFit->GetNDF();
-        TPaveText* pt = new TPaveText(0.0,0.9,0.20,1.0,"NDC");
-        pt->AddText(Form("N_{Gauss} = %d, #chi^{2}/NDF = %.2f, BIC = %.1f", bestN, chi2/ndf, bestBIC));
-        pt->SetFillColorAlpha(0,0);
+        TPaveText *pt = new TPaveText(0.02, 0.90, 0.25, 0.99, "NDC");
+        pt->AddText(Form("#chi^{2}/NDF = %.2f", sum->GetChisquare() / sum->GetNDF()));
+        pt->AddText(Form("N_{Gauss} = %d", nG));
+        pt->SetFillColorAlpha(0, 0);
         pt->Draw("same");
-        
 
-        c->Print("nSigmaTPC_plot.pdf");} 
-    
-    c->Print("nSigmaTPC_plot.pdf]");
+        c->Print("nSigmaTPC_theorySeed.pdf");
+
+        delete sum;
+        delete hist;
+    }
+
+    c->Print("nSigmaTPC_theorySeed.pdf]");
+    delete c;
 }
--- nSigma_Plot_pred.C
+++ nSigma_Plot_pred.C
@@ -56,157 +56,109 @@ std::vector<double> topBinCenters(TH1 *h, int nWanted)
    return xc;
 }
 
-void nSigma_Plot_pred(){
+void nSigma_Plot_pred(double pStart = 0.1, double pEnd = 2.0, double step = 0.1, double muWindow = 1.0)   
+{
     gROOT->SetBatch(kTRUE);
     gStyle->SetOptStat(1);
-    const char* baseDir = "/home/nfingerle/SMI/UD_LHC23_pass4_SingleGap/0106/B";
+
+    const char *baseDir = "/home/nfingerle/SMI/UD_LHC23_pass4_SingleGap/0106/B";
     TChain chain("twotauchain");
     AddTrees(chain, baseDir);
 
     chain.SetBranchStatus("*", 0);
     chain.SetBranchStatus("fTrkTPCinnerParam", 1);
-    chain.SetBranchStatus("fTrkTPCsignal", 1);
-    const char* subs[5] = {"El","Mu","Pi","Ka","Pr"};
-    for (int i = 0; i < 5; ++i) {
-        chain.SetBranchStatus(Form("fTrkTPCnSigma%s", subs[i]), 1);}
-    Float_t inner[2], signal[2];
+
+    const char *subs[5] = {"El", "Mu", "Pi", "Ka", "Pr"};
+    for (int i = 0; i < 5; ++i)
+        chain.SetBranchStatus(Form("fTrkTPCnSigma%s", subs[i]), 1);
+
+    Float_t inner[2];
     Float_t tpcNS[5][2];
     chain.SetBranchAddress("fTrkTPCinnerParam", inner);
-    chain.SetBranchAddress("fTrkTPCsignal", signal);
-    for (int i = 0; i < 5; ++i) {
-        chain.SetBranchAddress(Form("fTrkTPCnSigma%s", subs[i]), tpcNS[i]);}
+    for (int i = 0; i < 5; ++i)
+        chain.SetBranchAddress(Form("fTrkTPCnSigma%s", subs[i]), tpcNS[i]);
 
-    const Int_t nBins = 200;
-    const Double_t pMin = 0.2, pMax = 0.3; 
-    const Double_t xMin = -15.0, xMax = 15.0;
-    const Int_t nParts = 5;
-    const TString names[nParts] = {"e","#mu","#pi","K","p"};
-    const Int_t colors[nParts] = {kBlue, kBlueYellow, kGreen+2, kOrange+7, kViolet};
-    const Double_t resoTPC[5]   = {0.5, 0.075, 0.078, 0.09, 0.095}; 
-    const Double_t masses[nParts] = {0.00051099895, 0.1056583755,  0.13957039, 0.493677, 0.93827208816};
+    const Int_t   nBins   = 200;
+    const Double_t xMin   = -15.0, xMax = 15.0;
+    const Int_t   nParts  = 5;
+    const TString names[nParts]   = {"e", "#mu", "#pi", "K", "p"};
+    const Int_t   colors[nParts] = {kBlue, kBlueYellow, kGreen + 2,
+                                    kOrange + 7, kViolet};
+    const Double_t resoTPC[nParts] = {0.085, 0.072, 0.074, 0.09, 0.08}; 
+    const Double_t masses[nParts]  = {0.00051099895, 0.1056583755,
+                                      0.13957039,    0.493677,
+                                      0.93827208816};
 
-    TH1F *hRes[nParts];
-    for (int h = 0; h < nParts; ++h) {
-        hRes[h] = new TH1F(Form("n#sigma_{%s}",names[h].Data()), 
-        Form("n#sigma_{%s} for %.2f < p < %.2f GeV/c; n#sigma_{%s}; Counts", names[h].Data(),pMin, pMax, names[h].Data()),nBins, xMin, xMax);
-        hRes[h]->SetLineColor(colors[h]);
-        hRes[h]->SetMarkerColor(colors[h]);}
-    
-    Long64_t nEntries = chain.GetEntries();
-    nEntries = std::min(nEntries, static_cast<Long64_t>(1e6));
-    for(Long64_t i = 0; i < nEntries; ++i){
-        chain.GetEntry(i);
-        for(int j = 0; j < 2; ++j){
-            Float_t pG = inner[j]; 
-            if(pG < pMin || pG > pMax)   continue;
-            
-            for (int h = 0; h < nParts; ++h) {
-                if (!TMath::IsNaN(tpcNS[h][j])){
-                    hRes[h]-> Fill(tpcNS[h][j]);
+    Long64_t nEntries = std::min(chain.GetEntries(), static_cast<Long64_t>(1e6));
+
+    for (int ref = 0; ref < nParts; ++ref) {
+
+        TString pdfName = Form("nSigmaTPC_%s.pdf", names[ref].Data());
+        TCanvas *c = new TCanvas("c","n#sigma("+names[ref]+")",950,700);
+        c->SetLeftMargin(0.15); c->SetGrid(); c->SetLogy();
+        c->Print(pdfName+"[");
+
+        for (double pMin = pStart; pMin < pEnd; pMin += step) {
+            double pMax = pMin + step;
+            TH1F *h = new TH1F(Form("h_%s_%g",names[ref].Data(),pMin),
+                Form("n#sigma_{%s}, %.1f < p < %.1f GeV/c; n#sigma; Counts",
+                     names[ref].Data(), pMin, pMax),
+                nBins,xMin,xMax);
+            h->SetMarkerStyle(kFullCircle); h->SetMarkerSize(0.75);
+            h->SetMarkerColor(kBlack); h->SetLineColor(kBlack);
+
+            for(Long64_t ev=0; ev<nEntries; ++ev){
+                chain.GetEntry(ev);
+                for(int t=0;t<2;++t){
+                    float pG=inner[t];
+                    if(pG<pMin||pG>=pMax) continue;
+                    if(!TMath::IsNaN(tpcNS[ref][t])) h->Fill(tpcNS[ref][t]);
                 }
             }
+
+            double pMid = 0.5*(pMin+pMax);
+            double dRef = get_expected_signal(pMid*1000., masses[ref]*1000, 1.0);
+            std::vector<double> muSeed; std::vector<int> hypId;
+            double yMax = 1.05*h->GetMaximum();
+            for(int hyp=0; hyp<nParts; ++hyp){
+                double dHyp = get_expected_signal(pMid*1000., masses[hyp]*1000, 1.0);
+                if(dRef<0||dHyp<0) continue;
+                double mu = (dHyp/dRef -1.0)/resoTPC[hyp];
+                if(mu<xMin||mu>xMax) continue;
+
+                TLine *l=new TLine(mu,0,mu,yMax); l->SetLineColor(colors[hyp]); l->Draw();
+                TLatex tx; tx.SetTextColor(colors[hyp]); tx.SetTextSize(0.03); tx.SetTextAlign(22);
+                tx.DrawLatex(mu,0.92*yMax,names[hyp]);
+                muSeed.push_back(mu); hypId.push_back(hyp);
+            }
+            int nG = muSeed.size();
+            if(nG==0){ c->Print(pdfName); delete h; continue; }
+
+            std::ostringstream form; for(int i=0;i<nG;++i){ if(i) form<<"+"; form<<"gaus("<<3*i<<")"; }
+            TF1 *sum=new TF1("sum",form.str().c_str(),xMin,xMax);
+            for(int i=0;i<nG;++i){
+                int bin=h->FindBin(muSeed[i]); double amp=h->GetBinContent(bin);
+                sum->SetParameters(3*i, amp, muSeed[i], 0.30);
+                sum->SetParLimits(3*i+0, 0.0, h->GetMaximum()*1.2);
+                sum->SetParLimits(3*i+1, muSeed[i]-muWindow, muSeed[i]+muWindow);
+                sum->SetParLimits(3*i+2, 0, 100.0);
+            }
+            h->Fit(sum,"QR0");
+            c->Clear(); h->Draw("E1");
+            sum->SetLineColor(kRed); sum->SetLineWidth(2); sum->SetNpx(500); sum->Draw("same");
+            for(int i=0;i<nG;++i){ if(sum->GetParameter(3*i)<=0) continue;
+                TF1 *g=new TF1(Form("g_%d_%d",ref,i),"gaus",xMin,xMax);
+                g->SetParameters(&sum->GetParameters()[3*i]);
+                g->SetLineColor(colors[hypId[i]]); g->SetLineStyle(2); g->Draw("same"); }
+            for(size_t i=0;i<muSeed.size();++i){ TLine *l=new TLine(muSeed[i],0,muSeed[i],yMax); l->SetLineColor(colors[hypId[i]]); l->Draw(); }
+            TPaveText *pt=new TPaveText(0.02,0.90,0.25,0.99,"NDC");
+            pt->AddText(Form("#chi^{2}/NDF = %.2f",sum->GetChisquare()/sum->GetNDF()));
+            pt->AddText(Form("N_{Gauss} = %d",nG)); pt->SetFillColorAlpha(0,0); pt->Draw("same");
+
+            c->Print(pdfName);
+            delete sum; delete h;
         }
+        c->Print(pdfName+"]"); delete c;
     }
-
-    TCanvas *c = new TCanvas("c", "Residuals", 800, 600);
-    c->SetLeftMargin(0.15);
-    c->SetGrid();
-    c->Print("nSigmaTPC_plot.pdf[");
-    for (int h = 0; h < nParts; ++h) {
-        c->Clear();
-        hRes[h]->SetMarkerStyle(kFullCircle);
-        hRes[h]->SetMarkerColor(kBlack);
-        hRes[h]->SetLineColor(kRed);
-        hRes[h]->SetMarkerSize(0.75); 
-        hRes[h]->Draw("E1");
-
-        const double pMid = 0.5*(pMin + pMax);            
-        double mRef = masses[h]; 
-        for (int hyp = 0; hyp < 5; ++hyp) {
-            double mHyp = masses[hyp];
-            double dRef = get_expected_signal(pMid*1000, mRef*1000, 1.0);
-            double dHyp = get_expected_signal(pMid*1000, mHyp*1000, 1.0);                 
-
-            double nSigmaPeak = (dHyp/dRef - 1.0)/ (resoTPC[hyp]);
-            std::cout << "nSigmaPeak for " << subs[hyp] << " = " << nSigmaPeak << std::endl;
-            double yMax = 1.05 * hRes[h]->GetMaximum();
-            TLine *l = new TLine(nSigmaPeak, 0.0, nSigmaPeak, yMax);
-            l->SetLineColor(colors[hyp]);
-            l->SetLineStyle(1);
-            l->Draw("same");
-
-            TLatex txt;
-            txt.SetTextSize(0.035);
-            txt.SetTextAlign(22);                          
-            txt.SetTextColor(colors[hyp]);
-            txt.DrawLatex(nSigmaPeak, 0.9*yMax, subs[hyp]); 
-        }
-
-        const int nTryMax = 5;                         
-        TF1  *bestFit = nullptr;
-        double bestBIC = std::numeric_limits<double>::infinity();
-        int    bestN   = 0;
-
-        for (int nG = 1; nG <= nTryMax; ++nG) {
-
-            std::ostringstream form;
-            for (int i = 0; i < nG; ++i) {
-                if (i) form << "+";
-                form << "gaus(" << 3*i << ")";
-            }
-            TF1* fTmp = new TF1("fTmp", form.str().c_str(), xMin, xMax);
-
-            auto mu = topBinCenters(hRes[h], nG);
-            for (int i = 0; i < nG; ++i) {
-                int    bin = hRes[h]->FindBin(mu[i]);
-                double amp = hRes[h]->GetBinContent(bin);
-
-                fTmp->SetParameter(3*i+0, amp);          // amplitude  (p = 3i)
-                fTmp->SetParameter(3*i+1, mu[i]);        // mean       (p = 3i+1)
-                fTmp->SetParameter(3*i+2, 0.30);         // sigma      (p = 3i+2)
-
-                fTmp->SetParLimits(3*i+2, 0.05, 5.0);    
-            }
-
-            hRes[h]->Fit(fTmp, "QR0");                         
-            double chi2 = fTmp->GetChisquare();
-            int    k    = 3*nG;                                 
-            double nObs = hRes[h]->GetEntries();
-            double bic = chi2 +k * TMath::Log(nObs);
-            const double deltaBICmin = 2;
-
-            if (bestBIC - bic > deltaBICmin) {
-                bestBIC = bic;
-                bestN   = nG;
-                delete bestFit;
-                bestFit = (TF1*)fTmp->Clone(Form("best_%d",h));
-            }
-            else break;
-        }
-
-        bestFit->SetLineColor(kRed);
-        bestFit->SetNpx(500);
-        bestFit->SetLineWidth(2);
-        bestFit->Draw("same");
-
-        //for (int i = 0; i < bestN; ++i) {
-        //    TF1* g = new TF1(Form("g_%d_%d",h,i),"gaus",xMin,xMax);
-        //    for (int p = 0; p < 3; ++p)
-        //        g->SetParameter(p, bestFit->GetParameter(3*i+p));
-        //    g->SetLineStyle(2);
-        //    g->SetLineColor(colors[i % nParts]);
-        //    g->Draw("same");
-        //}
-
-        double chi2 = bestFit->GetChisquare();
-        int    ndf  = bestFit->GetNDF();
-        TPaveText* pt = new TPaveText(0.0,0.9,0.20,1.0,"NDC");
-        pt->AddText(Form("N_{Gauss} = %d, #chi^{2}/NDF = %.2f, BIC = %.1f", bestN, chi2/ndf, bestBIC));
-        pt->SetFillColorAlpha(0,0);
-        pt->Draw("same");
-        
-
-        c->Print("nSigmaTPC_plot.pdf");} 
-    
-    c->Print("nSigmaTPC_plot.pdf]");
 }
+
